f5a8cd4a8ab74ea8f66c476ced307b785dafa84f
Ana Santos <anaezes@gmail.com>
2018-05-20 14:20:16 +0100

    init loop of messages between client and server

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -5,13 +5,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 
 public class Client {
 
     private static HttpURLConnection con;
+
+    private static void makeHttpRequest(String url) throws IOException {
+        URL myurl = new URL(url);
+        con = (HttpURLConnection) myurl.openConnection();
+
+        con.setDoOutput(true);
+        con.setDoInput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "Java client");
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+    }
+
+    private static void makeConnectionToServer(String url, byte[] postData) throws IOException {
+
+        makeHttpRequest(url);
+
+        try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+            wr.write(postData);
+            wr.flush();
+            wr.close();
+        }
+
+        StringBuilder content;
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()))) {
+
+            String line;
+            content = new StringBuilder();
+
+            while ((line = in.readLine()) != null) {
+                content.append(line);
+                content.append(System.lineSeparator());
+            }
+            in.close();
+        }
+
+        System.out.println(content.toString());
+    }
 
     public static void main(String[] args) {
         String url = "http://localhost:8000/test";
@@ -19,43 +60,36 @@ public class Client {
         byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
 
         try {
+            //make request and read response
+            makeConnectionToServer(url, postData);
 
-            URL myurl = new URL(url);
-            con = (HttpURLConnection) myurl.openConnection();
+            Scanner scanner = new Scanner(System.in);
+            while(true) {
+                System.out.println("Enter something:");
+                String message = scanner.nextLine();
+                if(message.equals("exit")){
+                    break;
+                }
 
-            con.setDoOutput(true);
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", "Java client");
-            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                urlParameters = "message="+message;
+                postData = urlParameters.getBytes(StandardCharsets.UTF_8);
 
-            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-                wr.write(postData);
-            }
-
-            StringBuilder content;
-
-            try (BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()))) {
-
-                String line;
-                content = new StringBuilder();
-
-                while ((line = in.readLine()) != null) {
-                    content.append(line);
-                    content.append(System.lineSeparator());
+                //necessary - make new http request
+                url = "http://localhost:8000/chat";
+                makeHttpRequest(url);
+                try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                    wr.write(postData);
+                    wr.close();
                 }
             }
-
-            System.out.println(content.toString());
+            scanner.close();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-
             con.disconnect();
         }
     }
-
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -16,23 +16,46 @@ public class Server {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/test", new HelloHandler());
+        server.createContext("/chat", new MessageHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
+        while(true);
     }
 
-    static class MyHandler implements HttpHandler {
+    static class HelloHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
             InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
             BufferedReader br = new BufferedReader(isr);
             String query = br.readLine();
             int b;
             StringBuilder buf = new StringBuilder(512);
+            System.out.println("pim");
             while ((b = br.read()) != -1) {
                 buf.append((char) b);
             }
             System.out.println(query);
             byte [] response = "Welcome Real's HowTo test page".getBytes();
+            t.sendResponseHeaders(200, response.length);
+            OutputStream os = t.getResponseBody();
+            os.write(response);
+            os.close();
+        }
+    }
+
+    static class MessageHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String query = br.readLine();
+            int b;
+            StringBuilder buf = new StringBuilder(512);
+            System.out.println("pim");
+            while ((b = br.read()) != -1) {
+                buf.append((char) b);
+            }
+            System.out.println(query);
+            byte [] response = "Got your message".getBytes();
             t.sendResponseHeaders(200, response.length);
             OutputStream os = t.getResponseBody();
             os.write(response);
