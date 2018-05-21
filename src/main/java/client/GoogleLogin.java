eb4c009896e98c0f0b149b27a6b6d7db28a92f69
Fr0sk <the.fr0sk@gmail.com>
2018-05-21 22:53:25 +0100

    Implemented Google Login (WIP)

--- src/main/java/client/GoogleLogin.java
+++ src/main/java/client/GoogleLogin.java
@@ -14,60 +14,113 @@ import java.net.URISyntaxException;
 import java.util.Map;
 
 public class GoogleLogin {
-    private static final String clientID = "249929325899-euiefbmuh0b4vc50095vjl1okjmm703m.apps.googleusercontent.com";
-    private static final String clientSecret = "XdnV9XfwTdkt5xvKBMhv-fRu";
-    private static final String authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
-    private static final String tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
-    private static final String userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+    private final String clientID = "249929325899-kdf2nn2ndaemri7nk13r0d6ct8n9jtej.apps.googleusercontent.com";
+    private final String clientSecret = "N-Rx2Dt9aP3kGsfc0YXu0nkU";
+    private final String authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+    private final String tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+    private final String userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
 
-    public static void StartLogin() {
+    private final int port = 60217; //Utils.GetAvailablePort();
+    private final String baseRedirectUrl = "127.0.0.1:" + port;
+    private final String state = Utils.RandomDataBase64url(32);
+    private final String codeVerifier = Utils.RandomDataBase64url(32);
+    private final String codeChallenge = Utils.Base64urlencodeNoPadding(Utils.Sha256(codeVerifier));
+    private final String codeChallengeMethod = "S256";
+
+    private HttpServer server;
+
+    public GoogleLogin() {
+        // Creates HTTP Server
         try {
-            int port = Utils.GetAvailablePort();
-
-            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-            System.out.println("server started at " + port);
+            server = HttpServer.create(new InetSocketAddress(port), 0);
             server.createContext("/auth", new AuthorizationHandler());
             server.createContext("/token", new TokenHandler());
             server.setExecutor(null); // creates a default executor
-            server.start();
-            makeRequest(String.format("http://%s:%s/", "127.0.0.1", Integer.toString(port)));
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
         }
     }
 
-    private static void makeRequest(String redirectUrl) throws IOException, URISyntaxException {
+    public void StartLoginProcess() throws IOException, URISyntaxException {
+        server.start();
+        makeAuthRequest();
+    }
+
+    private void makeAuthRequest() throws IOException, URISyntaxException {
         // Creates the OAuth 2.0 authorization request.
-        String state = "ASOIJDAOsifjaoidjasoidJOSJAS";
-        String url = redirectUrl;
-        System.out.println(url);
 
-        String authorizationRequest = String.format("%s?response_type=code&scope=openid%%20profile&redirect_uri=%s&client_id=%s&state=%s",
+        /*String authorizationRequest = String.format("%s?response_type=code&scope=openid%%20profile&redirect_uri=%s&client_id=%s&state=%s",
                 authorizationEndpoint,
-                redirectUrl,
+                url,
                 clientID,
-                state);
+                state);*/
+        String authorizationRequestUrl = authorizationEndpoint + "?" +
+                "response_type=" + "code" +
+                "&scope=" + "openid%20profile" +
+                "&redirect_uri=" + getRedirectUrl(RedirectUrl.AUTH) +
+                "&client_id=" + clientID +
+                "&state=" + state +
+                "&code_challenge=" + codeChallenge +
+                "&code_challenge_method=" + codeChallengeMethod;
+
         if (Desktop.isDesktopSupported()) {
-            Desktop.getDesktop().browse(new URI(authorizationRequest));
+            Desktop.getDesktop().browse(new URI(authorizationRequestUrl));
         }
-        /*String authorizationRequest = String.format("{0}?response_type=code&scope=openid%20profile&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method={5}",
-                authorizationEndpoint,
-                new URL(redirectUrl).toString(),
-                clientID,
-                state,
-                code_challenge,
-                code_challenge_method);*/
     }
 
-    static class AuthorizationHandler implements HttpHandler {
+    private void performCodeExchange(String code) {
+        try {
+            String tokenRequestBody =
+                    "code=" + code +
+                    "&client_id=" + clientID +
+                    "&client_secret=" + clientSecret +
+                    "&redirect_uri=" + getRedirectUrl(RedirectUrl.TOKEN) +
+                    "&grant_type=authorization_code";
+
+            String result = Utils.SendPost(tokenEndpoint, tokenRequestBody);
+            System.out.println(result);
+
+        } catch (Exception e) {
+            System.err.println("Couldn't perform code exchange:\n" + e.getLocalizedMessage());
+        }
+    }
+
+    private String getRedirectUrl(RedirectUrl url) {
+        if (url.equals(RedirectUrl.AUTH)) {
+            return "http://" + baseRedirectUrl + "/auth";
+        } else if (url.equals(RedirectUrl.TOKEN)) {
+            return "http://" + baseRedirectUrl + "/token";
+        }
+        return baseRedirectUrl;
+    }
+
+    class AuthorizationHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
             Map<String, String> params = Utils.queryToMap(t.getRequestURI().getQuery());
+            //performCodeExchange(params.get("code"));
 
-            // TODO Generate
+            String response = "You can close your browser and return to the app\n" + codeVerifier + "\n\n";
+            for (int i = 0; i < params.size(); i++) {
+                response += params.keySet().toArray()[i] + ": " + params.get(params.keySet().toArray()[i]) + "\n";
+            }
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    class TokenHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            Map<String, String> params = Utils.queryToMap(t.getRequestURI().getQuery());
+            performCodeExchange(params.get("code"));
+
             String response = "";
+            for (int i = 0; i < params.size(); i++) {
+                response += params.keySet().toArray()[i] + ": " + params.get(params.keySet().toArray()[i]) + "\n";
+            }
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
@@ -75,20 +128,9 @@ public class GoogleLogin {
         }
     }
 
-    static class TokenHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            Map<String, String> params = Utils.queryToMap(t.getRequestURI().getQuery());
-
-            String response = String.format("%s %s, %s",
-                    params.get("code"),
-                    clientID,
-                    clientSecret);
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
+    private enum RedirectUrl {
+        AUTH,
+        TOKEN
     }
 
 }
--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,16 +1,31 @@
 package main.java.utils;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Utils {
-    public static int GetAvailablePort() throws IOException {
-        ServerSocket socket = new ServerSocket(0);
-        int port = socket.getLocalPort();
-        socket.close();
-        return port;
+    public static int GetAvailablePort() {
+        try {
+            ServerSocket socket = new ServerSocket(0);
+            int port = socket.getLocalPort();
+            socket.close();
+            return port;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     public static Map<String, String> queryToMap(String query){
@@ -24,5 +39,76 @@ public class Utils {
             }
         }
         return result;
+    }
+
+    public static String RandomDataBase64url(int length)
+    {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[length];
+        random.nextBytes(bytes);
+        return Base64urlencodeNoPadding(bytes);
+    }
+
+    public static String Base64urlencodeNoPadding(byte[] buffer)
+    {
+        String base64 = Base64.getEncoder().encodeToString(buffer);
+        // Converts base64 to base64url.
+        base64 = base64.replace("+", "-");
+        base64 = base64.replace("/", "_");
+        // Strips padding.
+        base64 = base64.replace("=", "");
+        return base64;
+    }
+
+    public static byte[] Sha256(String text) {
+        byte[] bytes = text.getBytes(StandardCharsets.US_ASCII);
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return digest.digest(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Algorithm not found: " + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+        return bytes;
+    }
+
+    public static String SendPost(String url, String bodyParams) {
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            con.setRequestProperty("Accept", "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            byte[] _byteVersion = bodyParams.getBytes(StandardCharsets.US_ASCII);
+            con.setRequestProperty("Content-Length", Integer.toString(_byteVersion.length));
+
+            //con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            //con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+            con.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(_byteVersion, 0, _byteVersion.length);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Post parameters : " + bodyParams);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.err.println(e);
+        }
+        return "";
     }
 }
